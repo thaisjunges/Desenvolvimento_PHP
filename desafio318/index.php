3e0a7b2f083c21f9a7c6179c5f4084536b4b2b47
Thais.Junges <ferreira.thais98@hotmail.com>
2022-12-01 11:11:16 -0300

    Desafio318

--- desafio318/index.php
+++ desafio318/index.php
@@ -4,6 +4,19 @@
     <title></title>
     </head>
     <body>
+        <?php 
+        
+        $nome = 'Thais';
+        $idade = 69;
+        $peso = 49;
+
+        if($idade >= 16 && $idade <= 69 && $peso >= 50){
+            echo 'Atende aos requisitos';
+
+        }else{
+            echo 'Não atende aos requisitos';
+        }
+        ?>
         
     </body>
 </html>
